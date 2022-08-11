ae0f9601cf61befd85b850391d106997247ac091
Caner <kurucaner@gmail.com>
2022-08-11 13:32:09 -0400

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,9 @@ const Home = () => {
   const { state, loading, error, setSearchTerm, searchTerm, setIsLoadingMore } =
     useHomeFetch();
 
-  if (error) return <div>Something went wrong...</div>
+  console.log(state);
+
+  if (error) return <div>Something went wrong...</div>;
   return (
     <>
       {state.results[0] ? (
@@ -45,7 +47,7 @@ const Home = () => {
       </Grid>
       {loading && <Spinner />}
       {state.page < state.total_pages && !loading && (
-        <Button text="Load More" callback={() => setIsLoadingMore(true)}/>
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
   );
